d4beaf6ddcb422c77208baf092896f7b153fdf39
Tybreen <tyler.breen@icloud.com>
2024-03-04 14:44:07 -0500

    Initial code and files

---
+++ script.js
@@ -0,0 +1,24 @@
+/* 
+# Pseudocode:
+
+Write out the message using console.log()
+
+Have three variables equal the corresponding combinations
+
+Output the combination using console.log()
+
+*/
+
+// Here to let the viewer know to look in the console.
+alert("Look at the console!")
+
+// Displaying the important information using console.log() which is written in the console.
+console.log("You have received this message because you have been chosen to open an important vault. Here is the secret combination:");
+
+// Using three variables that cannot be changed to create our three combinations.
+const digit_1 = 3 + 7;
+const digit_2 = 4 * 10;
+const digit_3 = 40 - 1;
+
+// Displaying our final combination.
+console.log(`${digit_1} - ${digit_2} - ${digit_3}`);
